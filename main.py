b881783fcb17626b34a8bdbc9d5c3ae8ff607a4d
Guto06 <agtomorato@gmail.com>
2024-08-15 14:23:14 -0300

    feat: adicionando o verifica o final do jogo correto

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+def Verificarfinal(matriz):
+    jogo = [
+
+        [1,3,5],
+        [2,4,6],
+        [7,8,'x']
+
+    ]   
+    if  matriz == jogo : 
+        print('Acabou')
+        print([matriz])
+        return 0
+ 
 matriz = [
 
     [1,3,5],
@@ -5,5 +18,4 @@ matriz = [
     [7,8,'x']
 
 ]   
-
-print([matriz])
+Verificarfinal(matriz)
